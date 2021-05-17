89aea1955e25320dffe5fc61bd5392f1ea60aef4
jimmydeac <83716031+jimmydeac@users.noreply.github.com>
2021-05-17 14:44:00 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    if (target <= 50) {
+        target = 100
+    }
+    basic.showNumber(target)
+})
+input.onButtonPressed(Button.B, function () {
+    if (target <= 100) {
+        target = 50
+    }
+    basic.showNumber(target)
+})
 let distance = 0
+let target = 0
 radio.setGroup(10)
 basic.forever(function () {
     distance = sonar.ping(
@@ -6,7 +19,7 @@ basic.forever(function () {
     DigitalPin.P1,
     PingUnit.Centimeters
     )
-    if (distance <= 20) {
+    if (distance <= 50) {
         basic.showIcon(IconNames.No)
     } else {
         basic.showIcon(IconNames.Yes)
