f3a0675131e030ece93e430bac42460d20da7f3d
jimmydeac <83716031+jimmydeac@users.noreply.github.com>
2021-05-17 14:55:41 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,16 +13,20 @@ input.onButtonPressed(Button.B, function () {
 let distance = 0
 let target = 0
 radio.setGroup(10)
+target = 50
+let is_detected = 0
 basic.forever(function () {
     distance = sonar.ping(
     DigitalPin.P0,
     DigitalPin.P1,
     PingUnit.Centimeters
     )
-    if (distance <= 50) {
+    if (distance <= target) {
         basic.showIcon(IconNames.No)
+        is_detected = 0
     } else {
         basic.showIcon(IconNames.Yes)
+        is_detected = 1
     }
-    radio.sendNumber(distance)
+    radio.sendNumber(is_detected)
 })
